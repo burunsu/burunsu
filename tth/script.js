6d7f04b5f1a0c40924633dc82c5c02d13ef63714
burunsu <136502226+burunsu@users.noreply.github.com>
2023-06-21 22:10:28 +0900

    Create script.js

---
+++ tth/script.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector("#navArea")
+let btn = document.querySelector(".toggle-btn")
+let msk= document.querySelector("#mask")
+
+btn.onclick = () => {
+  nav.classList.toggle("open");
+}
+
+mask.onclick = () => {
+  nav.classList.toggle("open");
+}
+
