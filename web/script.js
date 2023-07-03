2a2ddca0f4e785301cf9e26674da7b3accfbe5e2
burunsu <136502226+burunsu@users.noreply.github.com>
2023-07-03 23:33:20 +0900

    script.js の更新

--- web/script.js
+++ web/script.js
@@ -18,3 +18,14 @@ btn.onclick = () => {
 mask.onclick = () => {
   nav.classList.toggle("open");
 }
+console.log('Hello World!');
+//開くボタンを押した時には
+$(".open-btn").click(function() {
+  $("#search-wrap").addClass('panelactive'); //#search-wrapへpanelactiveクラスを付与
+  $('#search-text').focus(); //テキスト入力のinputにフォーカス
+});
+
+//閉じるボタンを押した時には
+$(".close-btn").click(function() {
+  $("#search-wrap").removeClass('panelactive'); //#search-wrapからpanelactiveクラスを除去
+});
