13563597e664e6123083c0a30c40a534382b50ca
burunsu <136502226+burunsu@users.noreply.github.com>
2023-07-09 15:02:35 +0900

    script.js の更新

--- web/script.js
+++ web/script.js
@@ -29,20 +29,4 @@ $(".open-btn").click(function() {
 $(".close-btn").click(function() {
   $("#search-wrap").removeClass('panelactive'); //#search-wrapからpanelactiveクラスを除去
 });
-(function() {
-  const image = document.querySelectorAll('.imgweep');
-
-  const observer = new IntersectionObserver(function(entries) {
-    entries.forEach(function(entry) {
-      if (entry.intersectionRatio > 0) {
-        entry.target.classList.add('img-animation');
-      } else {
-        entry.target.classList.remove('img-animation');
-      }
-    });
-  });
-
-  Array.prototype.forEach.call(image, function(img) {
-    observer.observe(img);
-  });
-})();
+   
